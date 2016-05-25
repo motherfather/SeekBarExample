2de8c05221d303719a67ccaabee1bffa9bdbff9d
kickscar <kickscar@gmail.com>
2016-05-25 16:46:46 +0900

    Initial commit

---
+++ app/src/main/java/com/estsoft/seekbarexample/MainActivity.java
@@ -0,0 +1,17 @@
+package com.estsoft.seekbarexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.DatePicker;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView( R.layout.activity_main );
+
+    }
+}
